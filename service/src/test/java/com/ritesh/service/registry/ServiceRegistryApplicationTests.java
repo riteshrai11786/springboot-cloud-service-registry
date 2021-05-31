efaa6087d0249464704137758206665988b503ff
Ritesh <riteshrai11786@gmail.com>
2021-05-31 19:14:33 +0530

    first commit

---
+++ service/src/test/java/com/ritesh/service/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ritesh.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
